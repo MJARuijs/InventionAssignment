fbf4f6247dda1dc55c9304f502dc2b596b8f3833
MJARuijs <mja.ruijs@gmail.com>
2019-03-27 15:33:04 +0100

    Extract the different devices and their data to seperate lists

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
 import data_reader
+import pylab as pl
 
 if __name__ == '__main__':
     data_dir = '/../res/data/'
-    data_reader.read_file(data_dir)
+    data = data_reader.read_file(data_dir)
+
+    devices = {}
+
+    for measurement in data:
+        device_name = measurement[0]
+        measurement_type = measurement[2]
+
+        if device_name not in devices:
+            devices[device_name] = {}
+            devices[device_name][measurement_type] = [measurement[1], measurement[3:]]
+        elif measurement_type not in devices[device_name]:
+            devices[device_name][measurement_type] = [measurement[1], measurement[3:]]
+        else:
+            devices[device_name][measurement_type].append([measurement[1], measurement[3:]])
+
+    for device_name, measurements in devices.items():
+        print(device_name)
+
+        for measurement_name, measurement in measurements.items():
+            print(measurement_name)
+            print(measurement)
+            print()
+        print()
+
