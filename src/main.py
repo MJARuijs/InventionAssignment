6ff950a3ac96ce9d4917b04df2029e42448bcf6c
MJARuijs <mja.ruijs@gmail.com>
2019-03-31 14:20:56 +0200

    Include some more data

--- src/main.py
+++ src/main.py
@@ -4,9 +4,23 @@ import Plotter
 
 
 if __name__ == '__main__':
-    all_data = FileReader.read_files('/../res/data/', 'free*.txt')
+    all_data = FileReader.read_files('/../res/data/', 'throw*.txt')
+
+    min_counts = []
+    data = []
+    delay = 0
+
     for file_data in all_data:
         time_array = np.linspace(0, file_data[1] * (file_data[0] / 1000), num=file_data[1])
-        Plotter.plot(time_array, file_data[2], 'Acceleration ' + file_data[5])
-        Plotter.plot(time_array, file_data[3], 'Magnetometer ' + file_data[5])
-        Plotter.plot(time_array, file_data[4], 'Gyroscope ' + file_data[5])
+        min_counts.append(file_data[1])
+        data.append([file_data[2], file_data[3], file_data[4], file_data[5]])
+        delay = file_data[0]
+
+    min_count = min(min_counts)
+
+    time_array = np.linspace(0, min_count * (delay / 1000), num=min_count)
+
+    for d in data:
+        Plotter.plot(time_array, d[0][0:min_count], 'Acceleration ' + d[3])
+        Plotter.plot(time_array, d[1][0:min_count], 'Magnetometer ' + d[3])
+        Plotter.plot(time_array, d[2][0:min_count], 'Gyroscope ' + d[3])
