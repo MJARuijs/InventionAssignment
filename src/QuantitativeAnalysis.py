cdf377f217e1ae1005ae237c04f8eb9a1eae2db3
MJARuijs <mja.ruijs@gmail.com>
2019-04-05 18:18:11 +0200

    Fix typo

--- src/QuantitativeAnalysis.py
+++ src/QuantitativeAnalysis.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
         key = d[3][:-4]
 
         if '\\' in key:
-            delimeter_index = key.index('\\')
-            key = key[delimeter_index, :]
+            delimiter_index = key.index('\\')
+            key = key[delimiter_index, :]
 
         truth_val = truth.get(key)
         print key, ": fall_pos: ", fall_pos,  " fall_time: ", fall_time, " truth_val: ", truth_val
